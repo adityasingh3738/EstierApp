ac62d1eb85e47babf10d3d40b4a14beffaf30cb5
Asmit Singh <asmitsingh@Asmits-MacBook-Air.local>
2025-10-16 13:35:37 +0530

    Make admin API routes public

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher([
   '/',
   '/api/tracks(.*)',
+  '/api/admin(.*)',
+  '/api/vote(.*)',
   '/sign-in(.*)',
   '/sign-up(.*)',
 ]);
